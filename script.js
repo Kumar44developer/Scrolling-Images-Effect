60f4911bf784d93aaadc347646f9b7aa9ca35925
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-12-23 14:24:08 +0530

    Calculated scroll threshold for card animation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const card = document.querySelectorAll(".card");
 window.addEventListener("scroll",animateCards);
 function animateCards(){
+    const checkingBottom = (window.innerHeight / 4) * (175/35);
 
